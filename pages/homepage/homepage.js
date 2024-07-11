97e1d1bfdff719dd1bedee4783ba1cdeade053e9
Fahad <fahadsaadalzahrani8@gmail.com>
2024-07-11 06:31:06 +0300

    90% Done

---
+++ pages/homepage/homepage.js
@@ -0,0 +1,18 @@
+const type = new Typed("#title", {
+    strings: [
+    "#Its <span>Healthier</span>",
+    "#Its <span>Faster</span>",
+    "#Its <span>Easier</span>",
+    "#Its <span>Caring</span>",
+    "#Its <span>Effective</span>",
+    "#Its <span>Simple</span>",
+    "#Its <span>Sustainable</span>",
+
+  ],
+  typeSpeed: 40,
+  smartBackspace: true,
+  startDelay: 5,
+  loop:true,
+  backSpeed: 20,
+  showCursor:false,
+});
---
+++ pages/learnmore/learnmore.js
@@ -0,0 +1,15 @@
+// const type = new Typed("#title", {
+//     strings: [
+//     "Learn about: <span>The Mission</span>",
+//     "Learn about: <span>The Vision</span>",
+//     "Learn about: <span>The Idea</span>",
+//     "Learn about: <span>The Team</span>",
+//     "Learn about: <span>The Structure</span>",
+//   ],
+//   typeSpeed: 40,
+//   smartBackspace: true,
+//   startDelay: 5,
+//   loop:true,
+//   backSpeed: 20,
+//   showCursor:false,
+// });
